73eb62a99a4fbce62bc9143fc40b03fab6884509
Jorge Cuevas <jorge.cuevas.92@gmail.com>
2017-11-22 02:32:07 -0600

    Added dimension usage based on kilometers as a dimension and increased accuracy to 84.87%

--- car-prices.py
+++ car-prices.py
@@ -30,18 +30,18 @@ y = np.array([
     24000])
 
 test = np.array([
-    [2011],
-    [2013],
-    [2001],
-    [2016],
-    [2012],
-    [2017],
-    [2011],
-    [2014],
-    [2008],
-    [2012],
-    [2015],
-    [2017]])
+    [2011, 25000],
+    [2013, 60000],
+    [2001, 5000],
+    [2016, 13569],
+    [2012, 15000],
+    [2017, 70],
+    [2011, 20000],
+    [2014, 60000],
+    [2008, 49233],
+    [2012, 72000],
+    [2015, 10000],
+    [2017, 4000]])
 print x
 print y
 print test
@@ -56,8 +56,8 @@ from sklearn import linear_model
 reg = linear_model.LinearRegression()
 reg.fit(test, y)
 
-print reg.predict([[2011]])
+print reg.predict([[2011, 23000]])
 print reg.score(test, y )
 
-pyplot.plot(test, reg.predict(test))
+pyplot.plot(x, reg.predict(test))
 pyplot.show()
